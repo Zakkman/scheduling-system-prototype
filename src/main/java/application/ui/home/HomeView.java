97227fe7c43de99a3495547ce0ff2e7ff97ea522
edesan <edesangerricjr@gmail.com>
2025-09-07 01:39:16 +0800

    update: updated home page + CSS added

--- src/main/java/application/ui/home/HomeView.java
+++ src/main/java/application/ui/home/HomeView.java
@@ -19,26 +19,32 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 public class HomeView extends VerticalLayout implements BeforeEnterObserver {
 
     public HomeView() {
+        setSizeFull();
         addClassName("home-view");
         add(createHeroSection());
     }
 
     private Component createHeroSection() {
+
         VerticalLayout heroSection = new VerticalLayout();
         heroSection.addClassName("hero-section");
 
         HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.addClassName("button-layout");
+        buttonLayout.addClassName("home-button-layout");
 
         H1 header = new H1("Welcome to the Automated Student-Teacher " +
             "\n Consultation Scheduling System");
+        header.addClassName("home-header");
         Span subHeader = new Span("Schedule and manage consultations with ease!");
+        subHeader.addClassName("home-sub-header");
         Button getStartedButton = new Button("Get Started", event -> {
             UI.getCurrent().navigate("register/user");
         });
+        getStartedButton.addClassName("home-get-started");
         Button loginButton = new Button("Login", event -> {
             UI.getCurrent().navigate("login");
         });
+        loginButton.addClassName("home-login");
 
         buttonLayout.add(getStartedButton, loginButton);
         heroSection.add(header, subHeader, buttonLayout);
